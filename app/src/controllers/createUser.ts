8001c52a0181bc1be14f34f3a58e2bbb815b6a86
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-08 11:36:59 -0500

    Added base code for createUser.ts

---
+++ app/src/controllers/createUser.ts
@@ -0,0 +1,13 @@
+
+import  asyncHandler from "express-async-handler";
+import { Request, Response } from "express";
+import { AppDataSource } from "../../src/data-source";
+import { User } from "../entities/User";
+
+const userRepository = AppDataSource.getRepository(User);
+
+export const createUser = asyncHandler( async (req : Request, res: Response) => {
+    const user = userRepository.create(req.body);
+    await userRepository.save(user);
+    res.status(201).json(user);
+});
