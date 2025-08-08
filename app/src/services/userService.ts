5517d6dc41dbd4ee33f03ec21d1f2fbc8ed1ada0
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-08 14:54:53 -0500

    Added base code for env.d.ts

--- app/src/services/userService.ts
+++ app/src/services/userService.ts
@@ -1,9 +1,10 @@
 import { AppDataSource } from "../data-source";
 import { User } from "../entities/User";
+import { IUserRegister } from "../interfaces/IUserRegister";
 const userRepository = AppDataSource.getRepository(User);
 
 
-async function create(body : any){
+async function create(body : IUserRegister){
     const user = userRepository.create(body as User);
     const createdUser = userRepository.save(user);
     return createdUser;
