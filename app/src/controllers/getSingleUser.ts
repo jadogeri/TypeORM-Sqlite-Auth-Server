483d893ac0913227ed7c22cdd3817baf3bda90d2
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-08 14:25:07 -0500

    Added Iemail.ts

--- app/src/controllers/getSingleUser.ts
+++ app/src/controllers/getSingleUser.ts
@@ -3,7 +3,7 @@ import  asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
 import * as userService from "../services/userService"
 
-export const registerUser = asyncHandler( async (req : Request, res: Response) => {
+export const getSingleUser = asyncHandler( async (req : Request, res: Response) => {
 
     const user = await userService.create(req.body);
 
