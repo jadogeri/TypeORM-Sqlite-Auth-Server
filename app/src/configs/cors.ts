e68b8a928b6133bbdef89a070d187817b56f9241
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-08-08 10:20:22 -0500

    Added corsOptions object in cors.ts

---
+++ app/src/configs/cors.ts
@@ -0,0 +1,7 @@
+export const corsOptions = {
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
+}
+
